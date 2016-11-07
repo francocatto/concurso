ea4fb7542940c6d99a7305d642be4085b03e27c5
fc <francocatto@gmail.com>
2016-11-07 18:39:09 -0300

    Arquivo que não havia sido versionado do commit anterior

---
+++ src/main/java/aplicacoes/Carro.java
@@ -0,0 +1,14 @@
+package aplicacoes;
+
+/**
+ * Created by fc on 07/11/16.
+ */
+public class Carro {
+
+    private String nome;
+
+
+    public Carro(String nome) {
+        this.nome = nome;
+    }
+}
